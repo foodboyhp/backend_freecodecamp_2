99bcb106b36b028b3a2f8ff6cb2e8e6145ccfe0d
HiepPham <hoanghieppham8b9@gmail.com>
2025-07-24 12:05:12 +0700

    send file path

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
-app.get("/", function(req, res) {
-  res.send("Hello Express");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 
