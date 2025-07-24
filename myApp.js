4b0700cd86507c75357c4554507895ba54bfc6c4
HiepPham <hoanghieppham8b9@gmail.com>
2025-07-24 12:14:58 +0700

    use express static

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let path = require('path');
 let app = express();
+
+// Serve static files from the "public" directory
+app.use('/public', express.static(__dirname + '/public'));
+
+// Set the view engine to EJS
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
