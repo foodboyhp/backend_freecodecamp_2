a4cbb99ac91c1627acce8ec67974c4b599a661ad
HiepPham <hoanghieppham8b9@gmail.com>
2025-07-24 13:43:19 +0700

    serve json on specific route

--- myApp.js
+++ myApp.js
@@ -2,15 +2,9 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-// Serve static files from the "public" directory
-app.use('/public', express.static(__dirname + '/public'));
-
-// Set the view engine to EJS
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('/json', (req, res) => {
+  res.json({ message: "Hello json" });
 });
-
-
 
 
 
